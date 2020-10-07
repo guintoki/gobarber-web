6d5207cdb6e6f751919074da96aae0bd164b85cf
guintoki <guilherme-vilas-boas@hotmail.com>
2020-10-06 23:19:52 -0300

    add style to login pag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
-const App: React.FC = () => <h1>hello world</h1>;
+
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;

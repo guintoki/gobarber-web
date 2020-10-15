604aa8c1c37931f81a00ca8f847d39d8af8c9c35
guintoki <guilherme-vilas-boas@hotmail.com>
2020-10-14 23:14:52 -0300

    create tooltip

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-use-before-define */
+import React from 'react';
+
+import { Container } from './styles';
+
+interface TooltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ title, children, className }) => {
+  return (
+    <Container className={className}>
+      {children}
+      <span>{title}</span>
+    </Container>
+  );
+};
+
+export default Tooltip;

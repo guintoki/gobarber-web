32908d572ac11db22e2e801c7c1e96bc4e5327c0
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-06 21:03:47 -0300

    add resetpassword

---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,117 @@
+/* eslint-disable camelcase */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-use-before-define */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable no-unused-expressions */
+import React, { useRef, useCallback } from 'react';
+import { FormHandles } from '@unform/core';
+import { FiLock } from 'react-icons/fi';
+import * as Yup from 'yup';
+import { Form } from '@unform/web';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import { useToast } from '../../hooks/Toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
+
+interface ResetPasswordFormData {
+  password_confirmation: string;
+  password: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const location = useLocation();
+
+  const handleSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Senha obrigatória'),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password')],
+            'Confirmação incorreta',
+          ),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro ao resetar a senha',
+          description: 'Ocorreu ao resetar a senha, tente novamente',
+        });
+      }
+    },
+    [addToast, history, location.search],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Resetar senha</h1>
+
+            <Input
+              icon={FiLock}
+              name="password"
+              type="password"
+              placeholder="Nova Senha"
+            />
+
+            <Input
+              icon={FiLock}
+              name="password_confirmation"
+              type="password"
+              placeholder="Confirmar Senha"
+            />
+
+            <Button type="submit">Alterar senha</Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;

bdf91cac6aa7050df05216446b648df1f4e8aca7
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-22 22:00:07 -0300

    add tests to input

---
+++ src/__tests__/components/Input.spec.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable no-use-before-define */
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import Input from '../../components/Input';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'email',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+
+describe('Input component', () => {
+  it('should be able to render an input', () => {
+    const { getByPlaceholderText } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+  });
+
+  it('should be able to rfocus input', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).not.toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).not.toHaveStyle('color: #ff9000;');
+    });
+  });
+
+  it('should keep input border when input is filled', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    fireEvent.change(inputElement, {
+      target: { value: 'guilherme@guintoki.com' },
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
+  });
+});
